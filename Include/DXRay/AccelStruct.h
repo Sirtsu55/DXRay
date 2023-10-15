9a6ad03daa472a294d31ddc7b8ba430133a949de
Srijan Dhungana <srijan.dhungana6@gmail.com>
2023-10-15 16:52:41 +0300

    merge accel struct class to one

--- Include/DXRay/AccelStruct.h
+++ Include/DXRay/AccelStruct.h
@@ -7,7 +7,7 @@ namespace DXR
 
     struct IAccelDesc
     {
-    public:
+    public: // Methods
         /// @brief Get the prebuild info for the acceleration structure.
         const D3D12_RAYTRACING_ACCELERATION_STRUCTURE_PREBUILD_INFO& GetPrebuildInfo() const { return PrebuildInfo; }
 
@@ -28,6 +28,30 @@ namespace DXR
         /// AllocateTopAccelerationStructure.
         bool HasBeenAllocated() const { return BuildDesc.Inputs.NumDescs > 0; }
 
+    public: // Members
+        /// @brief The flags to use when building the acceleration structure.
+        /// These must be set before calling AllocateTopAccelerationStructure or AllocateBottomAccelerationStructure,
+        /// because they are used to compute the prebuild info.
+        D3D12_RAYTRACING_ACCELERATION_STRUCTURE_BUILD_FLAGS Flags =
+            D3D12_RAYTRACING_ACCELERATION_STRUCTURE_BUILD_FLAG_NONE;
+
+        /// ------------------------------------------------------------------------
+        /// ------------------ Bottom Level Acceleration Structure -----------------
+        /// ------------------------------------------------------------------------
+
+        /// @brief The geometry to create the acceleration structure for.
+        /// This or pGeometries must be set for BLAS building. No need to fill both.
+        /// The pointers in this vector must be valid until the acceleration structure is built.
+        std::vector<D3D12_RAYTRACING_GEOMETRY_DESC> Geometries = {};
+
+        /// @brief The geometry to create the acceleration structure for, expressed as pointers.
+        /// This or Geometries must be set for BLAS building. No need to fill both.
+        std::vector<D3D12_RAYTRACING_GEOMETRY_DESC*> pGeometries = {};
+
+        /// ------------------------------------------------------------------------
+        /// ------------------- Top Level Acceleration Structure -------------------
+        /// ------------------------------------------------------------------------
+
     private:
         /// @brief The prebuild info for the acceleration structure.
         D3D12_RAYTRACING_ACCELERATION_STRUCTURE_PREBUILD_INFO PrebuildInfo = {};
@@ -38,30 +62,4 @@ namespace DXR
         friend class Device;
     };
 
-    /// @brief Contains the information needed to create a bottom-level acceleration structure.
-    /// @note Steps to create a bottom-level acceleration structure:
-    /// 1. Fill in either Geometries or pGeometries, and flags, other fields aren't required when allocating the
-    /// acceleration structure.
-    /// 2. Call AllocateBottomAccelerationStructure to get a handle to the acceleration structure.
-    /// 3. Fill in ScratchBuffer field.
-    /// 4. Call BuildBottomAccelerationStructure to build the acceleration structure.
-    struct BottomAccelDesc : public IAccelDesc
-    {
-        /// @brief The geometry to create the acceleration structure for.
-        /// This or pGeometries must be set. No need to fill both.
-        /// The pointers in this vector must be valid until the acceleration structure is built.
-        std::vector<D3D12_RAYTRACING_GEOMETRY_DESC> Geometries = {};
-
-        /// @brief The geometry to create the acceleration structure for, expressed as pointers.
-        /// This or Geometries must be set. No need to fill both.
-        std::vector<D3D12_RAYTRACING_GEOMETRY_DESC*> pGeometries = {};
-
-        /// @brief The flags to use when building the acceleration structure.
-        /// These must be set before calling AllocateBottomAccelerationStructure, because they are used to compute the
-        /// prebuild info.
-        D3D12_RAYTRACING_ACCELERATION_STRUCTURE_BUILD_FLAGS Flags =
-            D3D12_RAYTRACING_ACCELERATION_STRUCTURE_BUILD_FLAG_NONE;
-
-        friend class Device;
-    };
 } // namespace DXR
